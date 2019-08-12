e902f721277b426d84843b09dfc18af1ddc0dd5d
Luciano Ripani <lripani@gmail.com>
2019-08-11 21:40:26 -0300

    Push desde Eclipse de folders SRC y MVN generadas por Spring Initializr

---
+++ src/main/java/edu/utn/frro/ds/repuestos/RepuestosApplication.java
@@ -0,0 +1,13 @@
+package edu.utn.frro.ds.repuestos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepuestosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepuestosApplication.class, args);
+	}
+
+}
